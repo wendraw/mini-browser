13fb05d9923d9d8fdb75cba3a42286a1eb5dcdb1
wendraw <252798869@qq.com>
2022-03-20 17:04:22 +0800

    fix(shared): perfect Node type

--- packages/shared/src/ast.ts
+++ packages/shared/src/ast.ts
@@ -10,27 +10,31 @@ export interface Node {
 
 export interface DocumentNode extends Node {
   type: NodeTypes.DOCUMENT
+  children?: (ElementNode | TextNode)[]
 }
 
 export interface ElementNode extends Node {
   type: NodeTypes.ELEMENT
   attributes: Attribute[]
   tagName: string
-  parent?: ElementNode | null
-  children?: ElementNode[]
-  prev?: ElementNode | null
-  next?: ElementNode | null
+  parent?: ElementNode | DocumentNode | null
+  children?: (ElementNode | TextNode)[]
+  prev?: ElementNode | TextNode | null
+  next?: ElementNode | TextNode | null
 }
 
 export interface TextNode extends Node {
   type: NodeTypes.TEXT
   content: string
+  parent?: ElementNode | DocumentNode | null
+  prev?: ElementNode | TextNode | null
+  next?: ElementNode | TextNode | null
 }
 
 export interface Attribute {
   name: string
   value: string
-  specificity: Specificity
+  specificity?: Specificity
 }
 
 export type Specificity = [number, number, number, number]
